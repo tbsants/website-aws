e9f0645980b9e01b96d5e51fa05a06a3133d0a6d
tbsantos <tiagobs.santos@gmail.com>
2025-05-01 08:23:37 -0300

    Creating website infrastructure

--- sgs.tf
+++ sgs.tf
@@ -46,13 +46,13 @@ resource "aws_security_group" "sg_srv" {
     cidr_blocks = [var.cidr_block]
   }
 
-  # ingress {
-  #   description = "Allow access to 80 port"
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   cidr_blocks = [var.cidr_block]
-  # }
+  ingress {
+    description = "Allow access to 80 port"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
 
   ingress {
     description = "Allow access to 22 port"
