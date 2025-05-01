945179276b499cdb79beed6ed604cca38e4701f8
tbsantos <tiagobs.santos@gmail.com>
2025-05-01 08:40:57 -0300

    Creating website infrastructure

--- sgs.tf
+++ sgs.tf
@@ -38,29 +38,29 @@ resource "aws_security_group" "sg_srv" {
   description = "security group for website servers"
   vpc_id      = aws_vpc.this.id
 
-  # ingress {
-  #   description = "Allow access to 443 port"
-  #   from_port   = 443
-  #   to_port     = 443
-  #   protocol    = "tcp"
-  #   cidr_blocks = [var.cidr_block]
-  # }
+  ingress {
+    description = "Allow access to 443 port"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
 
-  # ingress {
-  #   description = "Allow access to 80 port"
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   cidr_blocks = [var.cidr_block]
-  # }
+  ingress {
+    description = "Allow access to 80 port"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
 
-  # ingress {
-  #   description = "Allow access to 22 port"
-  #   from_port   = 22
-  #   to_port     = 22
-  #   protocol    = "tcp"
-  #   cidr_blocks = [var.cidr_block]
-  # }
+  ingress {
+    description = "Allow access to 22 port"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
 
   egress {
     description = ""
@@ -83,26 +83,6 @@ resource "aws_security_group_rule" "allow_elb_to_srv" {
   security_group_id        = aws_security_group.sg_srv.id
   source_security_group_id = aws_security_group.sg_elb.id
   description              = "Allow ELB to access port 80 on server SG"
-}
-
-resource "aws_security_group_rule" "allow_https" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = [var.cidr_block]
-  security_group_id = aws_security_group.sg_srv.id
-  description       = "Allow access to 443 port"
-}
-
-resource "aws_security_group_rule" "allow_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = [var.cidr_block]
-  security_group_id = aws_security_group.sg_srv.id
-  description       = "Allow access to 22 port"
 }
 
 resource "aws_security_group" "sg_endpoint" {
