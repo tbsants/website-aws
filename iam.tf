b1726eeb1f89cbae0f854a4f91fdc46de8da270c
tbsantos <tiagobs.santos@gmail.com>
2025-05-25 07:49:13 -0300

    Criando iam role para ec2

---
+++ iam.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "EC2RoleForSSM" {
+  name = "EC2RoleForSSM"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+    tag-key = "EC2RoleForSSM"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "EC2RoleForSSMPolicy" {
+  role       = aws_iam_role.EC2RoleForSSM
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
